60a40ed6ec635deedf54a17d6b0b04e89780520e
Robinlemon <lewis-gibson@hotmail.com>
2022-08-07 19:27:49 +0100

    Add minecraft dns

--- dns.tf
+++ dns.tf
@@ -1,7 +1,15 @@
 resource "cloudflare_record" "dynmap" {
   zone_id = data.cloudflare_zone.zone.zone_id
   name    = "dynmap"
-  value   = "3.231.247.106"
   type    = "A"
-  proxied = true
+  value   = "3.231.247.106"
+  proxied = true # Take advantage of Cloudflare http caching
+}
+
+resource "cloudflare_record" "minecraft" {
+  zone_id = data.cloudflare_zone.zone.zone_id
+  name    = "minecraft"
+  type    = "A"
+  value   = "3.231.247.106"
+  proxied = false # The game server needs to be able to resolve the IP address.
 }
